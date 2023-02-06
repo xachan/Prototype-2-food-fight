0c6ab0a6814d2dd358ae2a43dab732c10d07d070
xachan <xachan@s.sfusd.edu>
2023-02-06 14:26:03 -0800

    challenge pts 1-6

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnLimitXLeft = -22.0f;
+    private float spawnLimitXRight = 7.0f;
+    private float spawnPosY = 30.0f;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -22,6 +22,10 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        // Selects ball for spawning
+
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(-spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
